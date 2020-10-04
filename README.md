5920b7bcc5098222c850b5acabf1e921ccaf2a12
pangeon <kamil.cecherz@gmail.com>
2020-10-04 19:19:51 +0200

    Piglet 0.1

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Programming with Vue.js - basic web applications
 
 * **Vue Dumb Calc** - simple Calculator usage raw HTML GUI input 
-* **Color Picker** - drawing pixel images 
+* **Color Picker** - drawing pixel images
+* **Piglet** - sexi app with pretty girls 
---
+++ piglet/main.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data:{
+        first: false,
+        second: false,
+        third: false,
+        fourth: false,
+        fiveth: false,
+        sixth: false,
+        seventh: false,
+        eighth: false,
+        nineth: false
+    }, 
+})
