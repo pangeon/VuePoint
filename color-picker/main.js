adb44dcbe6014b33b4d782b4d1772f3c5e0d8ae0
pangeon <kamil.cecherz@gmail.com>
2020-10-04 17:11:29 +0200

    Color picker 0.1.2

--- color-picker/main.js
+++ color-picker/main.js
@@ -2,12 +2,27 @@ new Vue({
     el: '#app',
     data:{
         headerHTML: '<h1>Color Picker</h1>',
+        invsibileStyle: 'display: none',
+        visibleStyle: 'display: inherit',
+        buttonDesc: 'About application',
+        contentOfInfoSection: '<span>Drawing pixel images. <br />Version 0.1 <br /> Author: <a href="http://www.cecherz.pl">Kamil Cecherz</a><span>'
     },
     methods:{
         draw: function(event, stance) {
             if(stance == undefined) {
                 event.target.classList.toggle('red');
             }
+        },
+        showInfoHiddenButton: function() {
+            this.invsibileStyle = this.visibleStyle;
+            this.visibleStyle = 'display: none';
+        },
+        hiddenInfoShowButton: function() {
+            this.invsibileStyle = 'display: none';
+            this.visibleStyle = 'display: inherit';
+        },
+        reset: function() {
+            window.location.reload();
         }
-    }
+    }    
 })
